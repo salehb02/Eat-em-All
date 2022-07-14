2881b016fa4d5f06ab6d0ff683787d8c154777f8
Saleh Bagheri <salehbagheri1381@gmail.com>
2022-07-14 15:38:51 +0430

    Add player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public VariableJoystick Joystick;
+    public float MovementSpeed = 2f;
+
+    private CharacterController _controller;
+    private Vector3 _smoothedDirection;
+
+    // Inputs
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Inputs();
+        Movement();
+    }
+
+    private void Inputs()
+    {
+        _horizontalInput = Joystick.Horizontal;
+        _verticalInput = Joystick.Vertical;
+    }
+
+    private void Movement()
+    {
+        _smoothedDirection = Vector3.Lerp(_smoothedDirection, new Vector3(_horizontalInput, 0, _verticalInput), Time.deltaTime * 5f);
+
+        _controller.SimpleMove(_smoothedDirection * MovementSpeed);
+    }
+}
